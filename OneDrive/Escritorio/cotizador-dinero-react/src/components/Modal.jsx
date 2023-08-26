04a21768682006dc2b7d8c137a063cb6b05aa5e9
JuanRaV <juanravirgen@gmail.com>
2023-08-26 17:11:59 -0600

    Adding categories and form of new expense

--- OneDrive/Escritorio/cotizador-dinero-react/src/components/Modal.jsx
+++ OneDrive/Escritorio/cotizador-dinero-react/src/components/Modal.jsx
@@ -1,8 +1,12 @@
 import CerrarBtn from '../img/cerrar.svg'
 
-const Modal = ({setModal}) => {
+const Modal = ({setModal,animarModal,setAnimarModal}) => {
     const ocultarModal = ()=>{
-        setModal(false)
+        
+        setAnimarModal(false)
+        setTimeout(()=>{
+            setModal(false)
+        },500)
     }
   return (
     <div className="modal">
@@ -13,6 +17,46 @@ const Modal = ({setModal}) => {
                 onClick={ocultarModal}
             />
         </div>
+        <form action="" className={`formulario ${animarModal ? 'animar': 'cerrar'}`}>
+            <legend>Nuevo Gasto</legend>
+            <div className='campo'>
+                <label htmlFor="nombre">Nombre Gasto: </label>
+                <input 
+                    id='nombre'
+                    type="text" 
+                    placeholder='Escribe el Nombre del Gasto'
+                />
+            </div>
+
+            <div className='campo'>
+                <label htmlFor="cantidad">Cantidad </label>
+                <input 
+                    id='cantidad'
+                    type="number" 
+                    placeholder='Escribe la Cantidad del Gasto: Ej.300'
+                />
+            </div>
+            <div className='campo'>
+                <label htmlFor="categoria">Categoria: </label>
+                <select 
+                    name="" 
+                    id="categoria"
+                >
+                    <option value="">--Seleccione--</option>
+                    <option value="ahorro">Ahorro</option>
+                    <option value="comida">Comida</option>
+                    <option value="casa">Casa</option>
+                    <option value="gastos">Gastos Varios</option>
+                    <option value="ocio">Ocio</option>
+                    <option value="salud">Salud</option>
+                    <option value="suscripciones">Suscripciones</option>
+                </select>
+            </div>
+            <input 
+                type="submit"
+                value="Añadir gasto"    
+            />
+        </form>
       
     </div>
   )
