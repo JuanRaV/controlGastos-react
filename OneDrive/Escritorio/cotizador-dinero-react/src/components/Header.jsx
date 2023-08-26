fa1e062c2a7aec55bb634badf596e596ff45486c
JuanRaV <juanravirgen@gmail.com>
2023-08-26 13:05:53 -0600

    Validation of budget

--- OneDrive/Escritorio/cotizador-dinero-react/src/components/Header.jsx
+++ OneDrive/Escritorio/cotizador-dinero-react/src/components/Header.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import NuevoPresupuesto from './NuevoPresupuesto'
 
-const Header = () => {
+const Header = ({
+    presupuesto,
+    setPresupuesto,
+    isValidPresupuesto,
+    setIsValidPresupuesto
+  }) => {
   return (
     <header>
         <h1>Planficador de Gastos</h1>
-        <NuevoPresupuesto/>
+        {/* Si el presupuesto es valido cargamos la siguiente pagina,
+        Si no se queda en la de definir presupuesto  */}
+        {isValidPresupuesto ? (
+          <p>Control Presupuesto</p>
+        ):(
+          <NuevoPresupuesto
+            presupuesto = {presupuesto}
+            setPresupuesto = {setPresupuesto}
+            setIsValidPresupuesto={setIsValidPresupuesto}
+          />
+        )}
+
     </header>
   )
 }
--- OneDrive/Escritorio/cotizador-dinero-react/src/components/NuevoPresupuesto.jsx
+++ OneDrive/Escritorio/cotizador-dinero-react/src/components/NuevoPresupuesto.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
+import { useState } from "react"
+import Mensaje from "./Mensaje"
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({
+  presupuesto,
+  setPresupuesto,
+  setIsValidPresupuesto
+  }) =>{
+  const [mensaje,setMensaje] = useState('')
+
+  //Validamos que el input sea un numero y no otra cosa
+  const handlePresupuesto = (e)=>{
+    e.preventDefault()
+    if(!Number(presupuesto) || Number(presupuesto)<0){
+      setMensaje('No es un presupuesto valido')
+      return
+    }
+    //Si tenemos un presupuesto valido:
+    setMensaje('')
+    setIsValidPresupuesto(true)
+    
+  }
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
-      <form action="" className='formulario'>
+      <form onSubmit={handlePresupuesto} action="" className='formulario'>
         <div className='campo'>
             <label >Definir Presupuesto</label>
             <input 
+                
                 className='nuevo-presupuesto'
                 type="text" 
                 placeholder='Anade tu Presupuesto'
+                value={presupuesto}
+                onChange={e=>setPresupuesto(e.target.value)}
             />
         </div>
         <input type="submit" value="Anadir" />
+        {mensaje && <Mensaje tipo ="error">{mensaje}</Mensaje>}
       </form>
     </div>
   )
